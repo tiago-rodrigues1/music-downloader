61229ff87282a8be9624f16f54bfea2032d8f60c
tiago-rodrigues1 <tiagords127@gmail.com>
2022-01-18 22:34:59 -0300

    fix: chamar funcoes p cada musica

--- index.js
+++ index.js
@@ -7,21 +7,20 @@ const downloader = require("./robots/downloader");
 const songsString = fs.readFileSync("./songs.txt").toString();
 const songs = songsString.split("\n");
 
-function run() {
+async function run() {
 	try {
-		songs.forEach(async (song) => {
+		for (let i = 0; i < songs.length; i++) {
 			const downloadPath = path
-				.resolve(__dirname, "my_songs", `${song}.mp3`)
+				.resolve(__dirname, "my_songs", `${songs[i]}.mp3`)
 				.replace(/ /gi, "_");
 
-			const youtubeVideoId = await searcher(song);
+			const youtubeVideoId = await searcher(songs[i]);
 			const downloadLink = await linkGetter(youtubeVideoId);
 
-			console.log(`- Downloading ${song}`);
-			downloader(downloadLink, downloadPath).then(() => {
-				console.log(`- ${song} [OK]`);
-			});
-		});
+			console.log(`- Downloading ${songs[i]}`);
+			await downloader(downloadLink, downloadPath);
+			console.log(`- ${songs[i]} [OK]`);
+		}
 	} catch (err) {
 		console.error(err);
 	}
